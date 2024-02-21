8f81ee4a0b6bcca40423273ff8789c8700b34dd2
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-21 11:13:24 -0500

    feat(uwu): make the rest of `uwu` scrpt

--- uwu.sh
+++ uwu.sh
@@ -2,6 +2,34 @@
 
 # TODO:
 # - Make a message
+# - load the -n param
 # - Format it
 
-echo "UWU"
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      NREP="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+printMessg(){
+    echo "UWU ->" $1
+}
+
+if [[ -z "$NREP" ]];then
+    NREP=1
+fi
+
+for (( n=1; n<=$NREP; n++ ))
+do
+    printMessg $n
+done
